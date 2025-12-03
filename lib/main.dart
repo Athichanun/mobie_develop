1502cd1005e1f1749622ae06a2e7fc922a5ae544
Athichanun <waveathichanun@gmail.com>
2025-12-03 11:14:02 +0700

    last commit first week

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: Text('My App'),
+//         backgroundColor: const Color.fromARGB(157, 254, 255, 191),
+//       ),
+//       body : Text('Hello test'),
+//     ),
+//   ));
+// }
+
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('My App'),
-      ),
-      body : Text('Hello test'),
-    ),
-  ));
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 120, 174, 255)),
+          useMaterial3: true,
+          appBarTheme:
+              const AppBarTheme(color: Color.fromARGB(255, 252, 240, 187))),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        ),
+        body: Text('Hello test',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w300)),
+      ),
+    );
+  }
+}
